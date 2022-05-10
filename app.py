ff342bd847dd2c4b89818d054e7c933713c52c36
Bruno Arcanjo <91852843+tcsystems-bruno-arcanjo@users.noreply.github.com>
2022-05-09 22:11:13 -0300

    Bug Removed

--- app.py
+++ app.py
@@ -8,14 +8,5 @@ csrf = CSRFProtect(app)
 def pagina_inicial():
     return "Receba! Obrigado meu Deus!"
 
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")
-
 if __name__ == '__main__':
     app.run()
